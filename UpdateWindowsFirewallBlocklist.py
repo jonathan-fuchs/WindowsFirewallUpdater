18c99a91a4d552367e4654095a08a3132482b16b
jonathan-fuchs <jgfuchs@proton.me>
2023-05-30 12:23:02 -0400

    moved repeated code into own method

--- UpdateWindowsFirewallBlocklist.py
+++ UpdateWindowsFirewallBlocklist.py
@@ -3,6 +3,17 @@ import requests, csv, subprocess, ipaddress
 #source=Abuse CH
 response = requests.get("https://feodotracker.abuse.ch/downloads/ipblocklist.csv").text
 
+def executeNewRules(listCount, ip_row, list_of_ips):
+    rule="netsh advfirewall firewall add rule name='BadIPOut"+str(listCount)+"' Dir=Out Action=Block RemoteIP="+ip_row
+    subprocess.run(["Powershell", "-Command", rule])
+    rule="netsh advfirewall firewall set rule name='BadIPOut"+str(listCount)+"' new RemoteIP="+list_of_ips
+    print("trying this rule",rule)
+    subprocess.run(["Powershell", "-Command", rule])
+    rule="netsh advfirewall firewall add rule name='BadIPIn"+str(listCount)+"' Dir=In Action=Block RemoteIP="+ip_row
+    subprocess.run(["Powershell", "-Command", rule])
+    rule="netsh advfirewall firewall set rule name='BadIPIn"+str(listCount)+"' new RemoteIP="+list_of_ips
+    subprocess.run(["Powershell", "-Command", rule])
+
 for i in range(0,100):
     test_rule = "netsh advfirewall firewall show rule name='BadIPOut"+str(i)+"' | findstr 'no rules'"
     test = (subprocess.run(["Powershell", "-Command", test_rule], capture_output=True, text=True).stdout)
@@ -39,26 +50,9 @@ for row in mycsv:
         print("Added Rule to block:",ip_row)
     else:
         counter=0
-        rule="netsh advfirewall firewall add rule name='BadIPOut"+str(listCount)+"' Dir=Out Action=Block RemoteIP="+ip_row
-        subprocess.run(["Powershell", "-Command", rule])
-        rule="netsh advfirewall firewall set rule name='BadIPOut"+str(listCount)+"' new RemoteIP="+list_of_ips
-        print("trying this rule",rule)
-        subprocess.run(["Powershell", "-Command", rule])
-        rule="netsh advfirewall firewall add rule name='BadIPIn"+str(listCount)+"' Dir=In Action=Block RemoteIP="+ip_row
-        subprocess.run(["Powershell", "-Command", rule])
-        rule="netsh advfirewall firewall set rule name='BadIPIn"+str(listCount)+"' new RemoteIP="+list_of_ips
-        subprocess.run(["Powershell", "-Command", rule])
+        executeNewRules(listCount, ip_row, list_of_ips)
         listCount+=1
         list_of_ips = ""
 
 if counter > 0:
-    rule="netsh advfirewall firewall add rule name='BadIPOut"+str(listCount)+"' Dir=Out Action=Block RemoteIP="+ip_row
-    subprocess.run(["Powershell", "-Command", rule])
-    rule="netsh advfirewall firewall set rule name='BadIPOut"+str(listCount)+"' new RemoteIP="+list_of_ips
-    print("trying this rule",rule)
-    subprocess.run(["Powershell", "-Command", rule])
-    rule="netsh advfirewall firewall add rule name='BadIPIn"+str(listCount)+"' Dir=In Action=Block RemoteIP="+ip_row
-    subprocess.run(["Powershell", "-Command", rule])
-    rule="netsh advfirewall firewall set rule name='BadIPIn"+str(listCount)+"' new RemoteIP="+list_of_ips
-    subprocess.run(["Powershell", "-Command", rule])
-    
+    executeNewRules(listCount, ip_row, list_of_ips)
